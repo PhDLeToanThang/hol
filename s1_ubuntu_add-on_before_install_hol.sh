8c303e3663e51c9a93f4d23011cf22f694581e10
PhD Le Toan Thang <106635733+PhDLeToanThang@users.noreply.github.com>
2024-01-09 16:16:52 +0700

    Update s1_ubuntu_add-on_before_install_hol.sh

--- s1_ubuntu_add-on_before_install_hol.sh
+++ s1_ubuntu_add-on_before_install_hol.sh
@@ -12,6 +12,7 @@ sudo systemctl enable xrdp
 sudo apt install ufw -y
 sudo apt install net-tools -y
 sudo apt install gparted -y
+# hỏi AI: Tôi muốn cài remmina qua snap của ubuntu như lại chọn kiểu silent thì dùng lệnh cli ntn?
 sudo snap install remmina --classic --quiet
 
 # Cách cấu hình điều khiển HĐH Linux qua Web HTML 5
