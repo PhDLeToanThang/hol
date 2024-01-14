0ae70d2ffe9710289d8b443f229b1d615ca83b5f
PhD Le Toan Thang <106635733+PhDLeToanThang@users.noreply.github.com>
2024-01-14 21:44:23 +0700

    Update s1_ubuntu_add-on_before_install_hol.sh

--- s1_ubuntu_add-on_before_install_hol.sh
+++ s1_ubuntu_add-on_before_install_hol.sh
@@ -13,7 +13,7 @@ sudo apt install ufw -y
 sudo apt install net-tools -y
 sudo apt install gparted -y
 # hỏi AI: Tôi muốn cài remmina qua snap của ubuntu như lại chọn kiểu silent thì dùng lệnh cli ntn ?
-sudo snap install remmina --classic
+# sudo snap install remmina --classic
 
 # Cách cấu hình điều khiển HĐH Linux qua Web HTML 5
 # https://thangletoan.wordpress.com/2022/05/22/cach-cau-hinh-dieu-khien-hdh-linux-qua-web-html-5/
@@ -21,5 +21,4 @@ sudo snap install remmina --classic
 sudo apt install -t ${VERSION_CODENAME}-backports cockpit -y
 
 # After you already have Cockpit on your server, point your web browser to: https://ip-address-of-machine:9090
-
 sudo apt install ubuntu-desktop -y
